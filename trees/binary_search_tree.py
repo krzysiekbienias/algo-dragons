f34527e7d6a43d732c0f33c34466aa963a6db02c
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2024-02-29 23:07:35 +0100

    Define class Node for tree. Implement BinarySearchTree together with insert method and method to visualize binary tree.

---
+++ trees/binary_search_tree.py
@@ -0,0 +1,77 @@
+from binarytree import tree
+import graphviz
+
+class Node:
+    def __init__(self,value,left=None,right=None) -> None:
+        self.value=value
+        self.left=left
+        self.right=right
+
+
+class BinarySearchTree:
+    """ class BinarySearchTree
+
+    Initialise empty binary search tree.
+    """
+    def __init__(self) -> None:
+        self.root=None
+
+    def insert(self,value:int):
+        new_node=Node(value)
+        if self.root is None:
+            self.root=new_node
+            return True
+        temp=self.root # assigne root to temp
+        while True:
+            if new_node.value==temp.value:
+                return False
+            if new_node.value<temp.value:
+                if temp.left is None:
+                    temp.left=new_node 
+                    return True
+                temp=temp.left 
+            
+            if new_node.value>temp.value:
+                if temp.right is None:
+                    temp.right=new_node
+                    return True
+                temp=temp.right
+
+
+
+
+
+def visualize_binary_tree(tree):
+    dot = graphviz.Digraph()
+    dot.node(str(tree.root.value))
+
+    def add_nodes_edges(node):
+        if node.left:
+            dot.node(str(node.left.value))
+            dot.edge(str(node.value), str(node.left.value))
+            add_nodes_edges(node.left)
+        if node.right:
+            dot.node(str(node.right.value))
+            dot.edge(str(node.value), str(node.right.value))
+            add_nodes_edges(node.right)
+
+    add_nodes_edges(tree)
+    dot.render('binary_tree', view=True, format='png')
+
+        
+
+        
+
+
+nodes=[7,18,56,23,44,25,26]
+my_tree=BinarySearchTree()
+for n in nodes:
+    my_tree.insert(n)
+
+visualize_binary_tree(my_tree)
+
+print("print the end")
+
+
+
+
