d78ceef9e6ee48af98e5d257dfd889dbece60060
Krzysiek Bienias <krzysiek.bienias.gmail.com>
2024-06-09 11:07:46 +0200

    implement linked list methods

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -1,41 +1,89 @@
 class Node:
-    def __init__(self,val,next=None) -> None:
-        
-        self.val=val
-        self.next=next
+    def __init__(self, val) -> None:
+        """
+        Description
+        -----------
+        Node object for linked list
 
+        Parameters
+        ----------
+        val : int
+        """
+
+        self.val = val
+        self.next = None
 
 
 class LinkedList:
-    def __init__(self,value) -> None:
-        
+
+    def __init__(self):
+        """
+        Description
+        -----------
+        Linked list without any Node.
+        """
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __str__(self):
+        temp_node=self.head
+        result=''
+        while temp_node is not None:
+            result=str(temp_node.value)
+            if temp_node.next is not None:
+                result+=' -> '
+            temp_node=temp_node.next
+        return result
+
+    def append(self, value: int):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def prepend(self,value:int):
         new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
 
-        self.head=new_node
-        self.tail=new_node
 
-        self.length=1
+class LinkedListWithOneNode:
+
+    def __init__(self, value) -> None:
+        """
+
+        Parameters
+        ----------
+        value :
+        """
+
+        new_node = Node(value)
+
+        self.head = new_node
+        self.tail = new_node
+
+        self.length = 1
 
     def print_linked_list(self):
-        temp=self.head
+        temp = self.head
         while self.head is not None:
-            print(str(temp.val)+"->")
-            temp=temp.next
-
-my_ll=LinkedList(4)
-my_ll.tail.next=Node(5)
-my_ll.tail=Node(5)
-my_ll.length+=1
-my_ll.tail.next=Node(8)
-my_ll.tail=Node(8)
-my_ll.length+=1
-my_ll.tail.next=Node(11)
-my_ll.tail=Node(11)
-my_ll.length+=1
-my_ll.tail.next=Node(47)
-my_ll.tail=Node(47)
-my_ll.length+=1
+            print(str(temp.val) + "->")
+            temp = temp.next
 
 
-print("the end")
-    
+if __name__ == '__main__':
+
+
+
+    print("the end")
