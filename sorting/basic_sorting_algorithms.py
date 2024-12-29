9f9cc82ebdc1b1ae7b52ccc8acb181720068c98c
krzysiekbienias <krzysztofbienias@Mac.lan>
2024-12-30 00:41:55 +0100

    Add bubble sort algorithms

--- sorting/basic_sorting_algorithms.py
+++ sorting/basic_sorting_algorithms.py
@@ -1,35 +1,80 @@
+from typing import List
+
+
+def bubble_sort(arr: List[int]):
+    changed=False
+    for u in range(len(arr)-1, 0, -1):
+        for i in range(u):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                changed=True
+        if not changed:
+            break
+
+
+
+
 def insertion_sorting(array):
     #cards analogy
-    for i in range(1,len(array)):
-        j=i
-        while j>0 and array[j]<array[j-1]:
-            array[j],array[j-1]=array[j-1],array[j]
-            j-=1
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+    return array
 
 
-def merge(left_array,right_array):
-    l,r,res=0,0,[]
-    while l<len(left_array) or r<len(right_array):
-        al=left_array[l] if l<len(left_array) else float('inf')
-        br=left_array[l] if l<len(left_array) else float('inf')
-        if al<br:
+def merge(left_array: List[int], right_array: List[int]) -> List[int]:
+    """
+    Description
+    Merge two sorted arrays into a new sorted array.
+    Parameters
+    ----------
+    left_array
+    right_array
+
+    Returns
+    -------
+
+    """
+    l, r, res = 0, 0, []
+    while l < len(left_array) or r < len(right_array):
+        al = left_array[l] if l < len(left_array) else float('inf')
+        br = right_array[r] if r < len(right_array) else float('inf')
+        if al < br:
             res.append(al)
-            l+=1
+            l += 1
         else:
             res.append(br)
-            r+=1
-        return res
+            r += 1
+    return res
 
-def merge_sort(array):
-    if len(array)<=1:
+
+def merge_sort(array: List[int]) -> List[int]:
+    """
+    Description
+    -----------
+    Merge two sorted arrays into a new sorted array.
+    Parameters
+    ----------
+    array
+
+    Returns
+    -------
+
+    """
+    if len(array) <= 1:
         return array
-    l=merge_sort(array[:len(array)//2])
-    r=merge_sort(array[len(array)//2:])
-    return merge(l,r)
+    l = merge_sort(array[:len(array) // 2])
+    r = merge_sort(array[len(array) // 2:])
+    return merge(l, r)
 
 
-
-if __name__=='__main__':
-    test_array=[3,1,5,6,7,2]
-    insertion_sorting(array=test_array)
-    print(test_array)
+if __name__ == '__main__':
+    test_array = [8,5,2,9,6,3]
+    print('Isertion sorting')
+    res=insertion_sorting(array=test_array)
+    print(res)
+    print('Merge sorting')
+    res2=merge_sort(array=test_array)
+    print(res2)
